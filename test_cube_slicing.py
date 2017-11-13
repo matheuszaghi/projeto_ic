1613037255f32f6a706d817db5c3758db9d9e3a9
Matheus Zaghi <matheus.zaghi.oliveira@usp.br>
2017-11-13 00:18:03 -0200

    Final version used to slice the cube

--- test_cube_slicing.py
+++ test_cube_slicing.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-
 Created on 01/11/2017
-
 @Author: Carlos Eduardo Barbosa
-
 Test routine for cube slicing
-
 """
 from __future__ import print_function, division
 
@@ -19,13 +15,20 @@ import context
 def test_slicing():
     """ Test slicing of the cube with MPDAF. """
     cubename = os.path.join(context.data_dir,
-                            "ADP.2017-06-16T13:59:19.244.fits")
+                            "ADP.2017-06-16T13_59_19.244.fits")
+
+
     cube = Cube(cubename, ext=1)
-    zdim, ydim, xdim = cube.shape
-    center = (ydim // 2, xdim // 2)
-    newcube = cube.select_lambda(cube.wave.get_range()[0], 7000)
+
+    center = (7.02162, 229.186)
+    lbda = (cube.wave.get_range()[0], 5900)
+
+    #gera um cubo 10x10 pixeis para teste do ppxf
+   
+    cube = cube.subcube(center, 2, lbda)
+
     newcubename = os.path.join(context.data_dir, "test_slice.fits")
-    newcube.write(newcubename)
+    cube.write(newcubename)
     return
 
 if __name__ == "__main__":
