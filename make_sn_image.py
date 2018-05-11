26725647a6504b111391cf0f501e5ec805756e40
cebarbosa <carlos.barbosa@usp.br>
2018-05-11 16:14:06 -0300

    Included program to make S/N image.

---
+++ make_sn_image.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+""" 
+
+Created on 11/05/18
+
+Author : Carlos Eduardo Barbosa
+
+"""
+
+import context
+
+import numpy as np
+from astropy.io import fits
+
+from der_snr import DER_SNR
+
+def make_sn_image():
+    """ Put short description here. """
+    pass
+
+if __name__ == "__main__":
+    make_sn_image()
