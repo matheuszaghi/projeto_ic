51dc94e9ab8b64064b2439ea5b6d1ef5223fe771
Matheus Zaghi <matheus.zaghi.oliveira@usp.br>
2018-05-15 17:56:08 -0300

    signal to noise

--- make_sn_image.py
+++ make_sn_image.py
@@ -9,14 +9,30 @@ Author : Carlos Eduardo Barbosa
 
 import context
 
+import os
+
 import numpy as np
+
 from astropy.io import fits
 
 from der_snr import DER_SNR
 
 def make_sn_image():
     """ Put short description here. """
-    pass
+    cubo = os.path.join(context.data_dir, "cube_10x10.fits")
+    flux = fits.getdata(cubo, 1)
+    #header = fits.getheader(cubo, 1)
+    #wave = (np.arange(header["naxis3"])) * header["cd3_3"] + \
+    #         header["crval3"]
+    
 
+    print(flux)
+    for arrayList in flux:
+
+        for array in arrayList:
+            snr = DER_SNR(array)
+            print(snr)
+
+    
 if __name__ == "__main__":
     make_sn_image()
